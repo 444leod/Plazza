69460cbd7f0c11aed1239fa10aed63e938ac6d42
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-05-14 20:18:08 +0200

    feat(argParsing): add basic arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,31 @@
 */
 
 #include "macros.hpp"
+#include <cstdint>
+#include <iostream>
 
-int main(UNUSED int argc, UNUSED char *argv[], UNUSED char *env[])
+int main(
+    UNUSED int argc,
+    char *argv[],
+    UNUSED char *env[]
+)
 {
+    double multiplier;
+    std::uint32_t nbCooks;
+    std::uint32_t restockTime;
+
+    try {
+        multiplier = std::stod(argv[1]);
+        nbCooks = std::stoi(argv[2]);
+        restockTime = std::stoi(argv[3]);
+    } catch (std::exception &e) {
+        return 84;
+    }
+    if (nbCooks <= 0)
+        return 84;
+
+    std::cout << "Multiplier: " << multiplier << std::endl;
+    std::cout << "Number of cooks: " << nbCooks << std::endl;
+    std::cout << "Restock time: " << restockTime << std::endl;
     return 0;
 }

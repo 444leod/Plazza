c2fa7889f4f103c6716311755dae141653f9eeca
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-05-14 15:26:49 +0200

    feat(Pizza): add Fantasia

---
+++ src/Pizzas/Fantasia.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** Pizzas
+** File description:
+** Fantasia
+*/
+
+#include "APizza.hpp"
+
+#pragma once
+
+namespace plz {
+    class Fantasia : public APizza{
+        public:
+            Fantasia(plz::PizzaSize size) :
+                _size(size),
+                _type(plz::PizzaType::Fantasia),
+                _ingredients({
+                    .dough = 1,
+                    .tomato = 1,
+                    .gruyere = 0,
+                    .ham = 0,
+                    .mushrooms = 0,
+                    .steak = 0,
+                    .eggplant = 1,
+                    .goatCheese = 1,
+                    .chiefLove = 1
+                }),
+                _bakingTime(2) {};
+            ~Fantasia();
+
+        private:
+            const plz::PizzaSize _size;
+            const plz::PizzaType _type;
+            const plz::Ingredrients _ingredients;
+            const std::uint32_t _bakingTime;
+    };
+}

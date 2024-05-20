57d33b41b147ae3cd4cc5153bc3f1110c974c057
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-20 16:48:32 +0200

    feat(Reception): added all reception class implementation.

--- src/Reception.hpp
+++ src/Reception.hpp
@@ -17,18 +17,20 @@
 namespace plz {
     class Reception {
         public:
-            Reception(plz::Setup setup, plz::IPizzaFactory factory);
+            Reception(plz::Setup& setup, plz::IPizzaFactory& factory);
             ~Reception() = default;
             void handleCommand(std::string command);
             void displayKitchenStatus();
 
         private:
-            void _addKitchen();
+            std::shared_ptr<plz::Kitchen> _addKitchen();
+            void _spreadPizzas(std::map<std::shared_ptr<plz::Kitchen>, std::pair<uint32_t, uint32_t>> kitchensStatus);
+            bool _queuePizza(std::map<std::shared_ptr<plz::Kitchen>, std::pair<uint32_t, uint32_t>> kitchensStatus, std::shared_ptr<plz::IPizza> pizza);
 
             uint32_t _pizzaiolos;
             uint32_t _restock;
             plz::IPizzaFactory _factory;
-            std::vector<plz::Kitchen> _kitchens;
-            std::vector<std::shared_ptr<plz::IPizza>> _pizzas;
+            std::vector<std::shared_ptr<plz::Kitchen>> _kitchens = {};
+            std::vector<std::shared_ptr<plz::IPizza>> _pizzas = {};
     };
 }

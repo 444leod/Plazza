f0c0e7509cacdff401e55f11b2c168e9a362799b
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-25 15:32:48 +0200

    update(Reception): updated reception to handle packets, with different packet types.

--- src/Reception.hpp
+++ src/Reception.hpp
@@ -11,6 +11,7 @@
 #include "Kitchen.hpp"
 #include "IPizza.hpp"
 #include "IPizzaFactory.hpp"
+#include "macros.hpp"
 
 #include <vector>
 
@@ -18,19 +19,34 @@ namespace plz {
     class Reception {
         public:
             Reception(plz::Setup& setup, plz::IPizzaFactory& factory);
-            ~Reception() = default;
+            ~Reception();
             void handleCommand(std::string command);
             void displayKitchenStatus();
+            void sendPacket(plz::Packet& packet);
+            void receivePackets();
+            void handlePackets();
+            void spreadPizzas();
 
         private:
             std::shared_ptr<plz::Kitchen> _addKitchen();
-            void _spreadPizzas(std::map<std::shared_ptr<plz::Kitchen>, plz::KitchenDatas> kitchensStatus);
-            bool _queuePizza(std::map<std::shared_ptr<plz::Kitchen>, plz::KitchenDatas>& kitchensStatus, std::shared_ptr<plz::IPizza> pizza);
+            bool _queuePizza(std::shared_ptr<plz::IPizza> pizza);
+            void _displayPrint(std::shared_ptr<plz::Kitchen>, std::shared_ptr<plz::Packet> packet);
+            void _displayStatus(std::shared_ptr<plz::Kitchen>, std::shared_ptr<plz::Packet> packet);
+            void _displayClosing(std::shared_ptr<plz::Kitchen>, UNUSED std::shared_ptr<plz::Packet> packet);
+            std::optional<plz::KitchenDatas> _getKitchenStatus(std::shared_ptr<plz::Kitchen> kitchen);
 
             uint32_t _pizzaiolos;
             uint32_t _restock;
             plz::IPizzaFactory _factory;
+            std::chrono::system_clock::time_point _lastOrder;
             std::vector<std::shared_ptr<plz::Kitchen>> _kitchens = {};
             std::vector<std::shared_ptr<plz::IPizza>> _pizzas = {};
+            std::vector<std::pair<std::shared_ptr<plz::Kitchen>, std::shared_ptr<plz::Packet>>> _kitchenPackets = {};
+            std::map<std::shared_ptr<plz::Kitchen>, plz::KitchenDatas> _kitchensStatus = {};
+            std::map<std::string, std::function<void(std::shared_ptr<plz::Kitchen>, std::shared_ptr<plz::Packet>)>> _displayFunctions = {
+                {"print",   [this](std::shared_ptr<plz::Kitchen> kitchen, std::shared_ptr<plz::Packet> packet) { this->_displayPrint(kitchen, packet); }},
+                {"status",  [this](std::shared_ptr<plz::Kitchen> kitchen, std::shared_ptr<plz::Packet> packet) { this->_displayStatus(kitchen, packet); }},
+                {"closing", [this](std::shared_ptr<plz::Kitchen> kitchen, std::shared_ptr<plz::Packet> packet) { this->_displayClosing(kitchen, packet); }}
+            };
     };
 }

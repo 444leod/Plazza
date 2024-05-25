70d1ceb193f8bbb0e05b658b2f702098e344ebac
Léo DUMONT <leo.dumont@epitech.eu>
2024-05-25 15:31:20 +0200

    feat(Runner): updated runner to handle packets, send correct packets to child.

--- src/Pizzas/APizza.hpp
+++ src/Pizzas/APizza.hpp
@@ -20,18 +20,18 @@ namespace plz {
                 _bakingTime(bakingTime) {};
             virtual plz::PizzaSize getSize() const { return _size; };
             virtual plz::PizzaType getType() const { return _type; };
-            virtual const plz::Ingredrients& getIngredients() const { return _ingredients; };
+            virtual const plz::Ingredients& getIngredients() const { return _ingredients; };
             virtual std::uint32_t getBakingTime() const { return _bakingTime; };
 
             virtual void setSize(PizzaSize size) { this->_size = size; }
             virtual void setType(PizzaType type) { this->_type = type; }
-            virtual void setIngredients(plz::Ingredrients& ingredients) { this->_ingredients = ingredients; }
+            virtual void setIngredients(plz::Ingredients& ingredients) { this->_ingredients = ingredients; }
             virtual void setBakingTime(std::uint32_t time) { this->_bakingTime = time; }
 
         protected:
             plz::PizzaSize _size;
             plz::PizzaType _type;
-            plz::Ingredrients _ingredients;
+            plz::Ingredients _ingredients;
             std::uint32_t _bakingTime;
     };
 }
